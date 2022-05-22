9c85241550bd30431ceea698ec3d20e17d61e712
zomrim <101129857+zomrim@users.noreply.github.com>
2022-05-22 03:23:20 +0300

    only comments remains

--- Main.c
+++ Main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 
 typedef struct StudentCourseGrade
 {
@@ -33,6 +34,7 @@ int* countRowTavs(char* buffer, int arr_size);
 Student* transformStudentArray(char*** students, const int* coursesPerStudent, int numberOfStudents);
 void writeToBinFile(const char* fileName, Student* students, int numberOfStudents);
 Student* readFromBinFile(const char* fileName);
+freeStudents(Student* students, int numberOfStudents);
 
 int main()
 {
@@ -42,17 +44,17 @@ int main()
 	char*** students = makeStudentArrayFromFile("studentList.txt", &coursesPerStudent, &numberOfStudents);
 	factorGivenCourse(students, coursesPerStudent, numberOfStudents, "Advanced Topics in C", +5);
 	printStudentArray(students, coursesPerStudent, numberOfStudents);
-	//studentsToFile(students, coursesPerStudent, numberOfStudents); //this frees all memory. Part B fails if this line runs. uncomment for testing (and comment out Part B)
+	/*studentsToFile(students, coursesPerStudent, numberOfStudents);*/ //this frees all memory. Part B fails if this line runs. uncomment for testing (and comment out Part B)
 
 	//Part B
 	Student* transformedStudents = transformStudentArray(students, coursesPerStudent, numberOfStudents);
 	writeToBinFile("students.bin", transformedStudents, numberOfStudents);
 	Student* testReadStudents = readFromBinFile("students.bin");
+	freeStudents(testReadStudents, numberOfStudents);
+	freeStudents(transformedStudents, numberOfStudents);
 
-	//add code to free all arrays of struct Student
 
-
-	/*_CrtDumpMemoryLeaks();*/ //uncomment this block to check for heap memory allocation leaks.
+	//_CrtDumpMemoryLeaks(); //uncomment this block to check for heap memory allocation leaks.
 	// Read https://docs.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2019
 
 	return 0;
@@ -222,6 +224,7 @@ void studentsToFile(char*** students, int* coursesPerStudent, int numberOfStuden
 
 		fputc('\n', fin);
 	}
+	free(students);
 	fclose(fin);
 }
 
@@ -338,3 +341,13 @@ int* countRowTavs(char* buffer, int arr_size)
 	return sizes;
 }
 
+freeStudents(Student* students, int numberOfStudents)
+{
+	Student* studentsPtr = students;
+	for (int i = 0; i < numberOfStudents; i++)
+	{
+		free(studentsPtr->grades);
+		studentsPtr++;
+	} 
+	free(students);
+}
